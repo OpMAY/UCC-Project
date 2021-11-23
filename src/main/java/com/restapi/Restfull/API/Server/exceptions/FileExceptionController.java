acae834892b92e279dcfcb95b669bc972e02d4dd
OpMAY <65110458+OpMAY@users.noreply.github.com>
2021-11-23 22:34:49 +0900

    File Limit Size Response ADD

--- src/main/java/com/restapi/Restfull/API/Server/exceptions/FileExceptionController.java
+++ src/main/java/com/restapi/Restfull/API/Server/exceptions/FileExceptionController.java
@@ -1,9 +1,12 @@
 package com.restapi.Restfull.API.Server.exceptions;
 
 import com.restapi.Restfull.API.Server.response.DefaultRes;
+import com.restapi.Restfull.API.Server.response.Message;
 import com.restapi.Restfull.API.Server.response.ResMessage;
 import com.restapi.Restfull.API.Server.response.StatusCode;
 import lombok.extern.log4j.Log4j2;
+import org.json.JSONException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,6 +17,9 @@ import org.springframework.web.multipart.MultipartException;
 @ControllerAdvice
 @Log4j2
 public class FileExceptionController {
+    @Value("${ucc.fileupload.limit}")
+    private long sizeLimit;
+
     @ExceptionHandler(MultipartException.class)
     protected ResponseEntity handleException(MultipartException e) {
         e.printStackTrace();
@@ -22,9 +28,11 @@ public class FileExceptionController {
     }
 
     @ExceptionHandler(MaxUploadSizeExceededException.class)
-    protected ResponseEntity handleException(MaxUploadSizeExceededException e) {
+    protected ResponseEntity handleException(MaxUploadSizeExceededException e) throws JSONException {
+        Message message = new Message();
+        message.put("size_limit", sizeLimit);
         e.printStackTrace();
         log.info(e.getMessage());
-        return new ResponseEntity(DefaultRes.res(StatusCode.FILE_MAX_SIZE_EXCEED, ResMessage.FILE_MAX_SIZE_EXCEED), HttpStatus.OK);
+        return new ResponseEntity(DefaultRes.res(StatusCode.FILE_MAX_SIZE_EXCEED, ResMessage.FILE_MAX_SIZE_EXCEED, message.getHashMap()), HttpStatus.OK);
     }
 }
